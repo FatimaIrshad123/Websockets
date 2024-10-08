4d9efa14456b9a38cc9ae3992390ba8f26be1011
FatimaIrshad123 <bibimemoona2017@gmail.com>
2024-10-08 10:00:30 +0500

    Updated code

--- websockets-nodejs/src/express.ts
+++ websockets-nodejs/src/express.ts
@@ -2,20 +2,17 @@ import express from 'express'
 import { WebSocketServer,WebSocket } from 'ws'
 
 const app = express()
-const httpServer = app.listen(8080)
+
+app.get("/", (req,res) => {
+    res.send("Hello World")
+})
+let httpServer = app.listen(8080)
 
 const wss = new WebSocketServer({ server: httpServer });
 
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
 
   ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
+    console.log('received: %s', data)
   });
-
-  ws.send('Hello! Message From Server!!');
 });
